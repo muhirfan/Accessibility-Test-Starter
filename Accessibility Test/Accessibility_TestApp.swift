7049b048fa5df206bdfacce234cccbfb4c384338
Muhammad Irfan <muhirfan.mi@gmail.com>
2025-06-25 00:55:07 +0700

    Initial Commit

---
+++ Accessibility Test/Accessibility_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_TestApp.swift
+//  Accessibility Test
+//
+//  Created by Muhammad Irfan on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
